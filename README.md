ff324147e71bdcf18f0f16dfb19e94f2892f6273
Schemen <schemen@users.noreply.github.com>
2022-03-21 15:39:43 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# sensehat-to-signalk
---
+++ sts.py
@@ -0,0 +1,34 @@
+import socket
+from time import sleep
+from sense_hat import SenseHat
+# Development
+#from sense_emu import SenseHat
+from datetime import datetime
+
+## CONFIG
+port = 30330
+tick = 1 # In seconds
+
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+sense = SenseHat()
+sense.clear()
+
+while True:
+
+  # Take readings from all three sensors
+  t = sense.get_temperature()
+  p = sense.get_pressure()
+  h = sense.get_humidity()
+  
+  # Create the message
+  # str() converts the value to a string so it can be concatenated
+  #message = "Temperature: " + str(t) + " Pressure: " + str(p) + " Humidity: " + str(h)
+  
+  signalk = '{"updates":[{"$source":"RaspiSenseHAT.Environment","timestamp":"'+ str(datetime.now()) +'","values":[{"path":"environment.0.temperature","value": '+ str(t) +'},{"path":"environment.0.relativeHumidity","value": '+ str(h) +'},{"path":"environment.0.pressure","value": '+ str(p) +'}]}]}'
+
+  # Display the scrolling message
+  #print(signalk)
+  sock.sendto(signalk.encode('utf-8'), ('127.0.0.1', port))
+  sleep(tick)
